ca2492127fdc2f9c4c0e6bdd8bd75d22b3ff52f4
Luis-Gdx <lg2696@hotmail.com>
2016-12-12 02:52:53 -0600

    agregar ordenamiento

--- public/js/app.js
+++ public/js/app.js
@@ -2,27 +2,26 @@ app = angular.module("app", ["app.directives"]);
 app.controller("mainCtrl", function($scope) {
     $scope.btn = [];
     $scope.newBtn = {};
-    var id = 1;
     $scope.add = function() {
-        $scope.newBtn.id = id;
-        $scope.newBtn.x = 0;
-        $scope.newBtn.y = 0;
+        //$scope.newBtn.id = id;
+        /*$scope.newBtn.x = 0;
+        $scope.newBtn.y = 0;*/
         $scope.btn.push($scope.newBtn);
         $scope.newBtn = {}
-        id++;
     }
-    $scope.position = function(id) {
-        $scope.newBtn.x = $("#c" + id).css("left");
-        $scope.newBtn.y = $("#c" + id).css("top");
-        $scope.newBtn.id = id;
-        //$scope.btn.splice(id - 1, 0, $scope.newBtn);
-        //$scope.newBtn = {};
+    $scope.position = function() {
+        for (var i = 0; i < $scope.btn.length; i++) {
+            $scope.newBtn.x = $("#c" + i).css("left");
+            $scope.newBtn.y = $("#c" + i).css("top");
+            $scope.btn[i] = $scope.newBtn;
+            $scope.newBtn = {};
+        }
         console.log($scope.btn);
-        //console.log("Input " + id + ": x = " + $scope.newBtn.x + " y = " + $scope.newBtn.y);
     }
     $scope.calc = function() {
-        for (var i = 0; i < $scope.btn.length; i++) {
-            console.log($scope.btn);
+    	var order = $scope.btn;
+        for (var i = 0; i < order.length; i++) {
+            console.log(order[i].x);
         }
     }
 });
