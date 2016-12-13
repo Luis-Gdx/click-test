1da3ca61bb848be140c741b0064e9c4e083cf2ca
Luis-Gdx <lg2696@hotmail.com>
2016-12-12 18:57:06 -0600

    Agregando el marco de los capacitores

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,10 @@
-app = angular.module("app", ["app.directives"]);
-app.controller("mainCtrl", function($scope) {
-    $scope.btn = [];
+app = angular.module("app", ["app.directives", "app.services"]);
+app.controller("mainCtrl", function($scope, Capacitor) {
+    $scope.btn = Capacitor.getAll();
     $scope.newBtn = {};
     $scope.add = function() {
-        //$scope.newBtn.id = id;
-        /*$scope.newBtn.x = 0;
-        $scope.newBtn.y = 0;*/
-        $scope.btn.push($scope.newBtn);
+        Capacitor.add($scope.newBtn);
         $scope.newBtn = {}
-    }
-    $scope.position = function() {
-        /*for (var i = 0; i < $scope.btn.length; i++) {
-            $scope.newBtn.x = $("#c" + i).css("left");
-            $scope.newBtn.y = $("#c" + i).css("top");
-            $scope.newBtn.id = i;
-            $scope.btn[i] = $scope.newBtn;
-            $scope.newBtn = {};
-        }*/
-        console.log($scope.btn);
     }
     $scope.calc = function() {
         for (var i = 0; i < $scope.btn.length; i++) {
@@ -28,17 +15,7 @@ app.controller("mainCtrl", function($scope) {
             $scope.newBtn = {};
         }
         var order = $scope.btn;
-        var aux;
-        for (var i = 0; i < order.length; i++) {
-            for (var j = 0; j < order.length - 1; j++) {
-                if (order[j].x > order[j + 1].x) {
-                    aux = order[j];
-                    order[j] = order[j + 1];
-                    order[j + 1] = aux;
-
-                }
-            }
-        }
+        order = Capacitor.sort(order);
         console.log(order);
     }
 });
--- public/js/services.js
+++ public/js/services.js
@@ -1,8 +1,38 @@
 angular.module("app.services", [])
-.factory("SetAttr", function () {
-	
-
-	return {
-
-	};
+.factory("Capacitor", function () {
+	var capacitor = {};
+	capacitor.input = [];
+	capacitor.add = function(input) {
+		capacitor.input.push(input);
+	}
+	capacitor.getAll = function() {
+		return capacitor.input;
+	}
+	capacitor.sort = function(array) {
+		var aux;
+		for (var i = 0; i < array.length; i++) {
+			for (var j = 0; j < array.length - 1; j++) {
+				if (array[j].x > array[j + 1].x) {
+					aux = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = aux;
+				}
+			}
+		}
+		for (var i = 0; i < array.length; i++) {
+			try{
+				console.log(":vvv " + i);
+				console.log(array[i + 1].y);
+				console.log(array[i].y);
+				console.log(array[i - 1].y);
+			}catch(err){}
+			if(array[i].y < array[i + 1] && array[i].y > array[i - 1]) {
+				array[i].parallel = false;
+			} else {
+				array[i].parallel = true;
+			}
+		}
+		return array;
+	}
+	return capacitor;
 })
